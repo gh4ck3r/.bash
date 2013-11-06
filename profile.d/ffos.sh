6afdb8105555c6a6cf816d2b9dcdf2b8867be49b
Ghacker <gh4ck3r@gmail.com>
2013-11-06 22:59:26 +0900

    function inherit_build_environment is removed
    It'll working only when ffos script is exist

--- profile.d/ffos.sh
+++ profile.d/ffos.sh
@@ -2,23 +2,21 @@
 
 export GREP_OPTIONS='--exclude=cscope.* --exclude-dir=.git'
 
-if [ -r ~/.ffos/ffos ];then source ~/.ffos/ffos; fi
+if [ -r ~/.ffos/ffos ];then
+	source ~/.ffos/ffos;
 
-function inherit_build_environment()
-{
-	local old_pwd=$PWD
-	while [ "$PWD" != "$HOME" -a "$PWD" != "/" ];do
-		if __ffos_is_b2g_dir $PWD;then
-			_ffos.at $PWD
-			clear
-			echo "B2G Build Environment is set at $PWD"
-			break;
-		fi
-		cd ..
-	done
-	cd $old_pwd
-}
-if [ ! -v ANDROID_BUILD_TOP ];then
-	inherit_build_environment
+	if [ ! -v ANDROID_BUILD_TOP ];then
+		__old_pwd=$PWD
+		while [ "$PWD" != "$HOME" -a "$PWD" != "/" ];do
+			if __ffos_is_b2g_dir $PWD;then
+				_ffos.at $PWD
+				clear
+				echo "B2G Build Environment is set at $PWD"
+				break;
+			fi
+			cd ..
+		done
+		cd $__old_pwd
+		unset __old_pwd
+	fi
 fi
-
