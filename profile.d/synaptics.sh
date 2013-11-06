c22e69ba3d816b9bb7497e3e759957059629f395
Changbin Park <changbin.park@lge.com>
2013-11-07 08:05:40 +0900

    Suppress error message from synclient
    The command exists even if there's no synaptics driver loaded.
    The condition should be changed to check the driver is loaded or not.

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -1,5 +1,5 @@
 # Synaptics TouchPad driver settings
 if [ synclient=$(which synclient) -a -x $synclient];then
 	# 2 finger scroll
-	synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8
+	synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
 fi
