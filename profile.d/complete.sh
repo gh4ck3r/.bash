da3b6f3ebf651e7c6b08e0416595408c75ccea99
Ghacker <gh4ck3r@gmail.com>
2013-12-11 23:30:48 +0900

    Utility function for bash completion is added
    bash function __complete_files_at can be used to complete filename
    recursively under given directory.

---
+++ profile.d/complete.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+################################################################################
+# function : __complete_files_at
+#   desc : complete files within given directory including subdirectory
+#   param1 : root directory for finding a files to be completed
+#
+function __complete_files_at()
+{
+  local rootdir=$(readlink -e ${1:-/})
+  local completing=${COMP_WORDS[COMP_CWORD]}
+
+  local finding=$rootdir$completing
+  local found=( $(compgen -f $finding) )
+  for f in ${found[@]};do
+    if [ $f = "$rootdir/." -o $f = "$rootdir/.." ];then continue; fi;
+    if [ -d $f ];then f+="/"; fi
+    COMPREPLY[${#COMPREPLY[@]}]=${f#$rootdir}
+  done
+
+
+  local basedir=${finding%/}
+  while [ "$basedir" != "$rootdir" -a $basedir != "/" ];do
+    basedir=$(dirname $basedir)
+  done
+
+  compopt -o filenames
+  if [ ${#COMPREPLY[@]} -ne 1 -o ! -f $basedir/${COMPREPLY[0]} ];then
+    compopt -o nospace
+  fi
+}
+
