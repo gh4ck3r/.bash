4ee4b60936f11f721bc91ca76dcbce980c77e00f
Changbin Park <changbin.park@lge.com>
2014-03-24 07:57:45 +0900

    Add some condition to prevent error messages.
    There's such a case that synclient is executable but no synaptic driver is
    loaded. So, I check the list of input devices and grep synaptic device.
    If nothing found, no execute synclient commands.

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -1,7 +1,9 @@
 # Synaptics TouchPad driver settings
-if [ synclient=$(which synclient) -a -x $synclient];then
-	# 2 finger scroll
-	synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
-  # Palm rejection
-  synclient PalmDetect=1 PalmMinWidth=6 PalmMinZ=100
+if [[ synclient=$(type -tfp synclient) && -x $synclient ]];then
+  if [[ $(xinput -list | grep "Synaptics TouchPad") ]];then
+    # 2 finger scroll
+    synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
+    # Palm rejection
+    synclient PalmDetect=1 PalmMinWidth=6 PalmMinZ=100 2>/dev/null
+  fi
 fi
