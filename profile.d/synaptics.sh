cffb81963eef4ea7ba08bde49e5d54f45f9c4da1
Changbin Park <gh4ck3r@gmail.com>
2014-03-23 20:49:11 +0900

    Add palm rejection for touchpad
    Configured for ultrabook gram that use ELAN touchpad.

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -2,4 +2,6 @@
 if [ synclient=$(which synclient) -a -x $synclient];then
 	# 2 finger scroll
 	synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
+  # Palm rejection
+  synclient PalmDetect=1 PalmMinWidth=6 PalmMinZ=100
 fi
