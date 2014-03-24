79ddc111f7e45228ad53e3c0f01dfdb6eac920ca
Changbin Park <changbin.park@lge.com>
2014-03-24 09:03:21 +0900

    Add semicolon to PROMPT_COMMAND smarter

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -9,5 +9,7 @@ function __update_git_branch_info()
   PS1+=$__PS1_SUFFIX;
 }
 
-command -v git >/dev/null 2>&1 &&
+if command -v git >/dev/null 2>&1 ;then
+  [[ "${PROMPT_COMMAND: -1}" != ';' ]] && PROMPT_COMMAND+=';'
   PROMPT_COMMAND+="__update_git_branch_info;"
+fi
