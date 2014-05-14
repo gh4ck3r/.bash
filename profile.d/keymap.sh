ba92985a7d1fd9366881537ba8065d75d1744f6d
Changbin Park <gh4ck3r@gmail.com>
2014-05-15 00:06:54 +0900

    Rebinding keymap for hangul/hanja for Ubuntu 14.04 on ultrabook(gram)

---
+++ profile.d/keymap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# vim: syntax=sh
+
+# This is only for Ubuntu 14.04 on my own gram
+if [[ $(uname -n) = "gh4ck3r" && $(grep DISTRIB_RELEASE= /etc/lsb-release | cut -f2 -d'=') = "14.04" ]];then
+  if which xmodmap > /dev/null 2>&1; then
+    xmodmap -e 'keycode 108 = Hangul'
+    xmodmap -e 'keycode 105 = Hangul_Hanja'
+    xmodmap -e 'remove mod1 = Hangul'
+    xmodmap -e 'remove control = Hangul_Hanja'
+  else
+    echo "xmodmap is not found --> Check hangul/hanja key remapping"
+  fi
+
+  if which gsettings > /dev/null 2>&1; then
+    gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['Hangul']"
+  else
+    echo "gsetting is not found --> Check hangul key binding"
+  fi
+fi
