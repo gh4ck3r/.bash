e7711967946220b8fedf54559868c8a72e53731f
Changbin Park <gh4ck3r@gmail.com>
2014-05-30 17:15:02 +0900

    Add middle click emulation with tap of 3 fingers

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -5,5 +5,7 @@ if [[ -x $(type -tfp synclient) ]];then
     synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
     # Palm rejection
     synclient PalmDetect=1 PalmMinWidth=6 PalmMinZ=100 2>/dev/null
+    # Tap with 3 finger emulates middle click
+    synclient ClickFinger3=2 2>/dev/null
   fi
 fi
