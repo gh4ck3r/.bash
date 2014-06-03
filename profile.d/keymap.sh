1b3b6593fd86388a467f4ee5844299ca2e359f47
Changbin Park <gh4ck3r@gmail.com>
2014-06-03 22:54:33 +0900

    Check Hangul/Hanja key remapping before modifying.
    This remove annoying message from xmodmap when launch new terminal with shortcut

--- profile.d/keymap.sh
+++ profile.d/keymap.sh
@@ -6,8 +6,19 @@ if [[ $(uname -n) = "gh4ck3r" && $(grep DISTRIB_RELEASE= /etc/lsb-release | cut 
   if which xmodmap > /dev/null 2>&1; then
     xmodmap -e 'keycode 108 = Hangul'
     xmodmap -e 'keycode 105 = Hangul_Hanja'
-    xmodmap -e 'remove mod1 = Hangul'
-    xmodmap -e 'remove control = Hangul_Hanja'
+    xmodmap -pm | while read mod_key rest; do
+      case "$mod_key" in
+        "mod1")
+          [[ "$rest" = *Hangul* ]] &&
+              xmodmap -e 'remove mod1 = Hangul'
+          ;;
+        "control")
+          [[ "$rest" = *Hangul_Hanja* ]] &&
+              xmodmap -e 'remove control = Hangul_Hanja'
+          ;;
+        *);;
+      esac
+    done
   else
     echo "xmodmap is not found --> Check hangul/hanja key remapping"
   fi
