9543ec73fcf509867a389ca3e8019d7c67919d3b
Changbin Park <gh4ck3r@gmail.com>
2014-08-27 21:46:31 +0900

    Configure palm detection

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -6,7 +6,7 @@ if [[ -z "$SSH_CLIENT" ]] && [[ -x $(type -tfp synclient) ]];then
     # 2 finger scroll
     synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
     # Palm rejection
-    synclient PalmDetect=1 PalmMinWidth=6 PalmMinZ=100 2>/dev/null
+    synclient PalmDetect=1 PalmMinWidth=5 PalmMinZ=80 2>/dev/null
     # Tap with 3 finger emulates middle click
     synclient ClickFinger3=2 2>/dev/null
   fi
