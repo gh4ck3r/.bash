10d7d44051be9a6a955529640ee16269a9655f4c
Changbin Park <gh4ck3r@gmail.com>
2015-02-25 22:29:51 +0900

    Prevent error due to xinput command
    In Raspberry pi, the command xinput is not exist by default.
    And Raspberry pi used to login into CUI that make xinput produce error message.

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -2,7 +2,7 @@
 
 # Skip if the session is on ssh
 if [[ -z "$SSH_CLIENT" ]] && [[ -x $(type -tfp synclient) ]];then
-  if xinput -list | grep -e "Synaptics TouchPad" -e "Elantech Touchpad" >/dev/null 2>&1 ;then
+  if xinput -list 2>/dev/null | grep -e "Synaptics TouchPad" -e "Elantech Touchpad" >/dev/null 2>&1 ;then
     # 2 finger scroll
     synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
     # Palm rejection
