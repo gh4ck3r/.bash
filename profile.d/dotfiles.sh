2e20c7c6fd74bf078a5817e66e6c38bc8a11e09a
Changbin Park <gh4ck3r@gmail.com>
2015-12-17 19:17:55 +0900

    Make links to dotfiles automatically

---
+++ profile.d/dotfiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function link_dot_files()
+{
+  local DOTFILES_ROOT=$__bashrc_dir/dotfiles
+  local shopt_cmd="shopt -p nullglob dotglob globstar"
+
+  local orig_shopt=$($shopt_cmd)
+
+  ${shopt_cmd/-p/-s}
+  for f in $DOTFILES_ROOT/**; do
+    if [[ -d $f ]]; then continue; fi
+
+    local rc_file_fullpath=${f#$DOTFILES_ROOT/}
+    if [[ ! -L ~/$rc_file_fullpath ]]; then
+      if [[ -e ~/$rc_file_fullpath ]]; then
+        echo "~/$rc_file_fullpath exists but not a link to $f"
+      else
+        echo "Making a symbolic link $rc_file_fullpath to $f"
+
+        local rc_file_dir=~/
+        if [[ $rc_file_fullpath != ${rc_file_fullpath##*/} ]];then
+          rc_file_dir+=${rc_file_fullpath%/*}
+        fi
+
+        ln -s -t $rc_file_dir $f
+      fi
+    fi
+
+  done
+  eval "$orig_shopt"
+}
+link_dot_files
+unset link_dot_files
