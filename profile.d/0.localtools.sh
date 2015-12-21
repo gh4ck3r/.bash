138f1770786a544ae7daf83e0088eb4dcf5cf98f
Changbin Park <changbin.park@lge.com>
2015-12-22 08:25:13 +0900

    [LocalTools] Add path settings for local tools
    Local tools are assumed in ~tools/**/bin
    "0" at the head of the script is for first execution among scripts in profile.d
    directory.

---
+++ profile.d/0.localtools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function setup_local_tools()
+{
+  local shopt_backup=$(shopt -p globstar)
+  shopt -s globstar
+  for bindir in ~/tools/**/bin;do
+    if [[ "$PATH" != *$bindir* ]];then
+      export PATH=$bindir:$PATH
+    fi
+  done
+  eval "$shopt_backup" 2> /dev/null
+}
+setup_local_tools
+unset setup_local_tools
