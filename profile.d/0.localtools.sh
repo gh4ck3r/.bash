69147b0ceb5ce48c4a665a5160e48f37e2e9ddfc
Changbin Park <changbin.park@lge.com>
2016-01-13 08:33:28 +0900

    Reduce searching depth of bin directory for local tools
    Changing pattern : ~/tools/**/bin --> ~/tools/*/bin
    In case of node.js, there're bunch of bin directories within it. They'll be
    linked as necessary.

--- profile.d/0.localtools.sh
+++ profile.d/0.localtools.sh
@@ -2,14 +2,14 @@
 
 function setup_local_tools()
 {
-  local shopt_backup=$(shopt -p globstar)
-  shopt -s globstar
-  for bindir in ~/tools/**/bin;do
+#  local shopt_backup=$(shopt -p globstar)
+#  shopt -s globstar
+  for bindir in ~/tools/*/bin;do
     if [[ "$PATH" != *$bindir* ]];then
       export PATH=$bindir:$PATH
     fi
   done
-  eval "$shopt_backup" 2> /dev/null
+#  eval "$shopt_backup" 2> /dev/null
 }
 setup_local_tools
 unset setup_local_tools
