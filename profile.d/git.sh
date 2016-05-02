eb6dba70eea117a5e7be121222394281a467edd9
Changbin Park <changbin.park@lge.com>
2016-05-02 15:47:06 +0900

    A function to setup git custom command is added

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-type git >/dev/null 2>&1 || return
+type -t git >/dev/null 2>&1 || return
 
 [[ -v PROMPT_COMMAND && "${PROMPT_COMMAND: -1}" != ';' ]] &&
   PROMPT_COMMAND+=';'
@@ -15,3 +15,45 @@ function __update_git_branch_info()
   PS1+=$__PS1_SUFFIX;
 }
 
+function setup_git_custom_commands()
+{
+  local git_custom_cmds=$__bashrc_dir/wrappers/git/git-*
+  [[ $(ls $git_custom_cmds 2>&- | wc -l) = 0 ]] && return
+
+  local git_exec_path=$(git --exec-path)
+  git_custom_cmds=($git_custom_cmds)
+  for cmd in $git_custom_cmds;do
+    if [[ $(readlink -e $git_exec_path/$(basename $cmd)) = $cmd ]]; then
+      git_custom_cmds=${git_custom_cmds#$cmd}
+    fi
+  done
+  [[ -z "$git_custom_cmds" ]] && return
+
+  local cmd_prefix;
+  local cmd_postfix;
+  if [[ -w $git_exec_path ]]; then
+    :
+  elif im_sudoer;then
+    cmd_prefix="sudo"
+  else
+    echo "Some custom git commands can't be installed"
+    for cmd in $git_custom_cmds;do
+      cmd=$(basename $cmd);
+      echo " - ${cmd/-/ }"
+    done
+    return
+  fi
+
+  for cmd in $git_custom_cmds;do
+    echo "Planting git custom command : $cmd --> $git_exec_path"
+
+    if $cmd_prefix ln -s $cmd $git_exec_path/;then
+      echo " -- Ok"
+    else
+      echo " -- Failed"
+    fi
+
+  done
+}
+setup_git_custom_commands
+unset setup_git_custom_commands
