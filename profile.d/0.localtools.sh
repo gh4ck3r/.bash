dcfceff83135f52882cf31dc95ad5f838c7a03a8
Changbin Park <changbin.park@lge.com>
2016-07-13 11:33:30 +0900

    Prevent ~/tools/*/bin to be added into PATH
    When there's no "bin" dir under ~/tools, for loop with variable "bindir" is
    iterated once with literal "~tools/*/bin". So, prevent such a case.

--- profile.d/0.localtools.sh
+++ profile.d/0.localtools.sh
@@ -5,7 +5,7 @@ function setup_local_tools()
 #  local shopt_backup=$(shopt -p globstar)
 #  shopt -s globstar
   for bindir in ~/tools/*/bin;do
-    if [[ "$PATH" != *$bindir* ]];then
+    if [[ -d $bindir ]] && [[ "$PATH" != *$bindir* ]];then
       export PATH=$bindir:$PATH
     fi
   done
