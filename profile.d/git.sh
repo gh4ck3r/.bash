d2ec90e7165db2ed44be64296d16b10806808ae6
Changbin Park <gh4ck3r@gmail.com>
2016-08-09 22:59:52 +0900

    Change branch symbol in PS1

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -11,7 +11,7 @@ function __update_git_branch_info()
   PS1=$__PS1_PREFIX;
   declare branch;
   branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null) &&
-    PS1+=':\[\e[01;93m\]('$branch')'
+    PS1+=':\[\e[01;93m\]⎇ '$branch
   PS1+=$__PS1_SUFFIX;
 }
 
