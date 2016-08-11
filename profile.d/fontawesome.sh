01a83183c6f8be62f0a1d5ce776994ef0c7819eb
Changbin Park <changbin.park@lge.com>
2016-08-11 10:49:37 +0900

    Add support Font Awesome as fallback font

---
+++ profile.d/fontawesome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check & install font-awesome if necessary
+function +FontAwesome()
+{
+  unset -f +FontAwesome
+  (fc-list | grep fontawesome-webfont\.ttf >/dev/null 2>&1) && return
+
+  echo "Font Awesome is not installed!"
+
+  if im_sudoer;then
+    local apt_get_cmd="sudo apt-get install fonts-font-awesome"
+    echo "Trying to install via '$apt_get_cmd'"
+    $apt_get_cmd
+  else
+    local download_url="https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/fontawesome-webfont.ttf"
+    local font_dir=~/.local/share/fonts;
+
+    mkdir -p $font_dir &&
+    wget -O $font_dir/$(basename $download_url) $download_url &&
+    fc-cache -v
+  fi
+}
+
+if ! +FontAwesome; then
+  echo "Failed to install Font Awesome!"
+  return
+fi
