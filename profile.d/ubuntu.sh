ff17e9aed4936951d6320b252c4f1a49e9101d16
Changbin Park <pchangbin@gmail.com>
2016-10-14 13:54:46 +0900

    Fix window resize via gsettings

--- profile.d/ubuntu.sh
+++ profile.d/ubuntu.sh
@@ -5,5 +5,5 @@
 # for 12.04
 gconftool-2 -s -t bool /apps/metacity/general/resize_with_right_button true
 # and for 12.10/13.04
-[[ -x gsettings ]] && gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+[[ -x $(which gsettings) ]] && gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 
