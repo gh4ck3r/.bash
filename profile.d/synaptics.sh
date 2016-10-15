59c7f43d15c29b34cfc8d2413f46f29fe6e008c2
Changbin Park <pchangbin@gmail.com>
2016-10-15 21:37:53 +0900

    Ignore Left/RightEdge to prevent click by palm

--- profile.d/synaptics.sh
+++ profile.d/synaptics.sh
@@ -14,5 +14,18 @@ xinput -list 2>/dev/null |
 synclient HorizTwoFingerScroll=1 VertTwoFingerScroll=1 EmulateTwoFingerMinZ=40 EmulateTwoFingerMinW=8 2>/dev/null
 # Palm rejection
 synclient PalmDetect=1 PalmMinWidth=5 PalmMinZ=10 2>/dev/null
+# Ignore Left/Right edge since those're used to be clicked by palm
+if [[ $(synclient -V) < 1.9 ]]; then
+  # Apply values of 'LeftEdge' and 'RightEdge' to 'AreaLeftEdge' and
+  # 'AreaRightEdge' respectively
+  if [[ $(hostname) = 'cpark-xps' ]];then
+    synclient LeftEdge=48 RightEdge=1168
+  fi
+  synclient $(synclient | grep -e '\<\(Left\|Right\)Edge\>' | sed -e 's/\(\w\+\)\s*=\s*\([0-9]\+\)/Area\1=\2/') 2>/dev/null
+else
+  # AreaLeftEdge and AreaRightEdge should be specified in percent from
+  # synaptics 1.9 and later(See man page for detail)
+  echo "Failed to ignore left/right edge area of touchpad" >&2
+fi
 # Tap with 3 finger emulates middle click
 #synclient ClickFinger3=2 2>/dev/null
