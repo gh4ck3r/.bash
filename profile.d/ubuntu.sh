5dba02dc31985b0f6527f2676073f0d281aaeb87
Changbin Park <gh4ck3r@gmail.com>
2016-10-19 21:53:48 +0900

    Enable minimize window with unity icon

--- profile.d/ubuntu.sh
+++ profile.d/ubuntu.sh
@@ -5,5 +5,9 @@
 # for 12.04
 gconftool-2 -s -t bool /apps/metacity/general/resize_with_right_button true
 # and for 12.10/13.04
-[[ -x $(which gsettings) ]] && gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
-
+if [[ -x $(which gsettings) ]];then
+  # Enable window resize with mouse right click
+  gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+  # Enable minimize/restore window with unity icon
+  gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+fi
