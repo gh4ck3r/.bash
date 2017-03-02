d09872cf7444acd911033813d1ffa2d9098c0904
Changbin Park <pchangbin@gmail.com>
2017-03-02 13:12:34 +0900

    Cut trail new line when copy to clipboard

--- profile.d/clipboard.sh
+++ profile.d/clipboard.sh
@@ -4,7 +4,7 @@
 #  $ ls | clipboard
 
 if type -t xclip 2>&1 >/dev/null ;then
-  alias clipboard='xclip -selection clipboard';
+  alias clipboard='(xclip && echo -n $(xclip -o) | xclip -selection clipboard)';
 else
   echo "xclip is not installed";
 fi
