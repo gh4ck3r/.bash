582f67a200ccbbff8f0f4ed03471cf36810d32b3
Changbin Park <pchangbin@gmail.com>
2017-03-13 16:06:26 +0900

    Use refactored source-list.sh

--- profile.d/cscope.sh
+++ profile.d/cscope.sh
@@ -1,208 +1,14 @@
 #!/bin/bash
 
-declare -a CSCOPE_TARGET_EXT=(
-    '*.webidl' '*.aidl' '*.ipdl' '*.idl'
-    '*.xml' '*.xbl' '*.html'
-    '*.mk'
-    '*.js' '*.jsm' '*.json'
-    '*.java'
-    '*.h' '*.H' '*.hh' '*.hpp' '*.hxx'
-    '*.c' '*.C' '*.cc' '*.cpp' '*.c++' '*.cxx'
-    '*.s' '*.S'
-    '*.x'
-    '*.y'
-    '*.php' '*.php3' '*.phtml');
-# Generate find parameter to find targets
-_CSCOPE_TARGET_FIND_PATTERN="-type f ( ";
-set -f
-for _ptrn in ${CSCOPE_TARGET_EXT[@]}; do
-	_CSCOPE_TARGET_FIND_PATTERN+="-iname $_ptrn -o ";
-done
-set +f
-unset CSCOPE_TARGET_EXT
-_CSCOPE_TARGET_FIND_PATTERN="${_CSCOPE_TARGET_FIND_PATTERN%-o } ) -print";
-
 function _cscope_build_db()
 {
-	local at=$1
   local gen_db_cmd="cscope -bqk"
 
-  if [[ -x "$(which gtags)" ]];then
-    gen_db_cmd="gtags -f cscope.files"
-    echo "Using gtags(global) instead of cscope"
-  fi
 	echo -n "Generate cscope DB" && (
-		pushd $at > /dev/null 2>&1 &&
+		pushd $1 > /dev/null 2>&1 &&
 		$gen_db_cmd &&
 		popd > /dev/null 2>&1
 	) && echo " -- Done" || echo " -- Failed"
-}
-
-function _cscope-unknown-at()
-{
-	local at=$1
-	local list_file="$at/cscope.files"
-  if [ -r $list_file ];then
-    echo "Use existing list file : $list_file"
-  else
-    echo "Generate file list for cscope at $at"
-    set -f
-    find $at $_CSCOPE_TARGET_FIND_PATTERN > "$list_file"
-    set +f
-  fi
-
-	_cscope_build_db $at
-}
-
-function _cscope-linux-at()
-{
-	local at=$1
-	local list_file="$at/cscope.files"
-
-  if [ -r $list_file ];then
-    echo "Use existing file list of linux kernel for cscope at $list_file"
-  else
-    echo "Generate file list of linux kernel for cscope at $at"
-    set -f
-    find $at \
-      -type d \
-        -path "$at/include/asm-*" \
-        ! -path "$at/include/asm-arm*" \
-        ! -path "$at/include/asm-generic*" \
-        -prune -o \
-      -type d \
-        -path "$at/arch/*" \
-        ! -path "$at/arch/arm*" \
-        -prune -o \
-      $_CSCOPE_TARGET_FIND_PATTERN > "$list_file"
-    set +f
-  fi
-	_cscope_build_db $at
-}
-
-function _cscope-gaia-at()
-{
-	local at=$1
-	local list_file="$at/cscope.files"
-  if [ -r $list_file ];then
-    echo "Use existing file list of gaia for cscope at $list_file"
-  else
-    echo "Generate file list of gaia for cscope at $at"
-    set -f
-    find $at \
-      -path "$at/xulrunner-sdk*" -prune -o \
-      -path "$at/xulrunner" -prune -o \
-      $_CSCOPE_TARGET_FIND_PATTERN > "$list_file"
-    set +f
-  fi
-
-	_cscope_build_db $at
-}
-
-function _cscope-gecko-at()
-{
-	local at=$1
-	local list_file="$at/cscope.files"
-  if [ -r $list_file ];then
-    echo "Use existing file list of gecko for cscope at $list_file"
-  else
-    echo "Generate file list of gecko for cscope at $at"
-    set -f
-    find $at \
-      -path "$at/python" -prune -o \
-      -path "$at/*test" -prune -o \
-      -path "$at/*tests" -prune -o \
-      -path "$at/*testing" -prune -o \
-      -path "$at/*gtk2" -prune -o \
-      -path "$at/*mswindows" -prune -o \
-      $_CSCOPE_TARGET_FIND_PATTERN > "$list_file"
-    set +f
-  fi
-
-	_cscope_build_db $at
-}
-
-function _cscope-b2g-at()
-{
-	local at=$1
-
-	shift;
-	if [ $# -eq 0 ];then
-		echo "cscope databases for b2g are beging generated."
-		[[ -d $at/gecko  ]] && _cscope-gecko-at $at/gecko
-		[[ -d $at/gaia   ]] && _cscope-gaia-at  $at/gaia
-		[[ -d $at/kernel ]] && _cscope-linux-at $at/kernel
-	else
-		while [ $# -gt 0 ];do
-			case "$1" in
-				gecko)
-					_cscope-gecko-at $at/gecko
-					;;
-				gaia)
-					_cscope-gaia-at  $at/gaia
-					;;
-				kernel)
-					_cscope-linux-at $at/kernel
-					;;
-				*)
-					echo "Unknown cscope target : $1"
-					;;
-			esac
-			shift
-		done
-	fi
-}
-
-function _cscope-nodejs-prj-at()
-{
-  local at=$1
-	local list_file="$at/cscope.files"
-  if [ -r $list_file ];then
-    echo "Use existing file list of nodejs project for cscope at $list_file"
-  else
-    echo "Generate file list of nodejs project for cscope at $at"
-
-    set -f
-    find $at \
-      -path "$at/node_modules" -prune -o \
-      $_CSCOPE_TARGET_FIND_PATTERN > "$list_file"
-    set +f
-  fi
-
-	_cscope_build_db $at
-}
-
-function _cscope-determine-sourcetree()
-{
-	local target=$1
-	if [ ! -d $target -o ! -x $target ];then
-		echo "$target is inaccessible"
-		return;
-	fi
-
-  # define source type and it's subdirectories here
-	declare -A source_dirs=(
-		[linux]="arch block crypto drivers firmware fs include init ipc kernel lib mm net samples scripts security sound usr virt"
-		[gecko]="accessible b2g browser build caps chrome config content db docshell dom editor embedding extensions gfx hal image intl ipc js layout media memory mfbt mobile modules mozglue netwerk nsprpub other-licenses parser probes profile python rdf security services startupcache storage testing toolkit tools uriloader view webapprt widget xpcom xpfe xulrunner"
-		[gaia]="apps build dogfood_apps external-apps locales media-samples shared showcase_apps test_apps test_external_apps test_media tests tools"
-		[b2g]="abi build development gaia hardware librecovery ndk prebuilt system bionic device external gecko kernel vendor bootable frameworks gonk-misc libcore build/envsetup.sh"
-    [nodejs-prj]="node_modules"
-	)
-	for type in ${!source_dirs[@]};do
-		local found=true
-		for f in ${source_dirs[$type]};do
-			if [ ! -s "$target/$f" ];then
-				found=false;
-				break;
-			fi
-		done
-		if $found;then
-			echo "$type"
-			return
-		fi
-	done
-	echo "unknown"
-	return
 }
 
 function cscope-at()
@@ -214,23 +20,21 @@ function cscope-at()
 	elif [ ! -w "$at" ];then echo "$1 is not writable";    return 3;
 	fi
 
-	local target=$(_cscope-determine-sourcetree "$at")
-	local handler=_cscope-$target-at
+  local listfile="$at/cscope.files";
+  if [[ -r "$listfile" ]];then
+    echo "Existing $listfile is used"
+  else
+    local sources=$($__bashrc_dir/tools/source-list.sh "$at");
+    rm -f $listfile;  # To make sure empty
+    for f in $sources;do  # Add only when the sources is readable
+      [[ -r $f ]] && echo "$f" >> "$listfile";
+    done
+  fi
 
-	if [ "$(type -t $handler)" = "function" ];then
-		set -f
-		if $handler $at;then
-			local db=$at/cscope.out
-			if [ -f $db ] && [[ "$CSCOPE_DB" != *$db* ]];then
-				export CSCOPE_DB+=${CSCOPE_DB:+:}$db;
-			fi
-		fi
-		set +f
-		return 0
-	fi
+  _cscope_build_db "$at"
 
-	cat <<EOF >&2
-The source at $at is recognized as '$target' but no handler for it.
-EOF
-	return 255
+  local db="$at/cscope.out"
+  if [ -f $db ] && [[ "$CSCOPE_DB" != *$db* ]];then
+    export CSCOPE_DB+=${CSCOPE_DB:+:}$db;
+  fi
 }
