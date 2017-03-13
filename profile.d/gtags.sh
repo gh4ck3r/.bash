a3537590fc66ac5e344f7b2e86d035cd4335df1b
Changbin Park <pchangbin@gmail.com>
2017-03-13 16:13:04 +0900

    Seperate gtags profile from cscope

---
+++ profile.d/gtags.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function _gtags_build_db()
+{
+	echo -n "Generate gtags DB" && (
+		pushd $1 > /dev/null 2>&1 &&
+		gtags &&
+		popd > /dev/null 2>&1
+	) && echo " -- Done" || echo " -- Failed"
+}
+
+function gtags-at()
+{
+	local at=$(readlink -e ${1:-$PWD});
+
+	if   [ ! -d "$at" ];then echo "$1 is not a directory"; return 1;
+	elif [ ! -x "$at" ];then echo "$1 is not accessible";  return 2;
+	elif [ ! -w "$at" ];then echo "$1 is not writable";    return 3;
+	fi
+
+  local listfile="$at/gtags.files";
+  if [[ -r "$listfile" ]];then
+    echo "Existing $listfile is used"
+  else
+    local sources=$($__bashrc_dir/tools/source-list.sh "$at");
+    rm -f $listfile;  # To make sure empty
+    for f in $sources;do  # Add only when the sources is readable
+      [[ -r $f ]] && echo "$f" >> "$listfile";
+    done
+  fi
+
+  _gtags_build_db "$at"
+  local db="$at/cscope.out"
+  if [ -f $db ] && [[ "$CSCOPE_DB" != *$db* ]];then
+    export CSCOPE_DB+=${CSCOPE_DB:+:}$db;
+  fi
+}
