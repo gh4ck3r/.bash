898330bb1b41e71f8fa515873d523e9029843468
Changbin Park <pchangbin@gmail.com>
2017-04-13 10:07:18 +0900

    Add auto completion for command "tizen"

--- profile.d/tizen.sh
+++ profile.d/tizen.sh
@@ -12,8 +12,10 @@ function set_tizen_env()
   local tools=$TIZEN_SDK/tools;
   local sdb=$tools/sdb;
   local sdb_completion=$tools/.sdb-completion.bash;
+  local tizen_completion=$tools/ide/bin/tizen-autocomplete;
 
   [[ -r $sdb_completion ]] && . $sdb_completion;
+  [[ -r $tizen_completion ]] && . $tizen_completion;
 
   [[ -x $sdb ]] && [[ -w $sdb ]] && ! [[ -L $sdb ]] || return;
   echo -e "\033[91;1mReplace sdb with wrapper\033[0m : $sdb"
