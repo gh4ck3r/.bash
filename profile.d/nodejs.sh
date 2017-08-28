ec84891e9fb3a3d82566877fe0b5a212b1e04587
Changbin Park <pchangbin@gmail.com>
2017-08-28 11:06:19 +0900

    Make global node modules available with require()

--- profile.d/nodejs.sh
+++ profile.d/nodejs.sh
@@ -7,8 +7,11 @@ function set_nodejs_env()
   node_dirs=$(ls -d /opt/nodejs/node-v* | sort -r) # let higher version first
   for nodejs_root in $node_dirs;do
     local nodejs_bin_dir=$nodejs_root/bin
+    local nodejs_lib_dir=$nodejs_root/lib/node_modules
     if [[ -x $nodejs_bin_dir/node ]];then
       [[ $PATH != *$nodejs_bin_dir* ]] && export PATH=$nodejs_bin_dir:$PATH
+      [[ $NODE_PATH != *$nodejs_lib_dir* ]] &&
+        export NODE_PATH+=${NODE_PATH:+:}$nodejs_lib_dir
       type -P npm >/dev/null 2>&1 && eval "$(npm completion)"
       break;
     fi
