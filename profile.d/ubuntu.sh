7ae1183ce1840f8dadd3198059000efdcfcfc13d
Changbin Park <changbin_park@mcafee.com>
2018-05-25 14:45:00 +0900

    Add settings for Ubuntu 18.04

--- profile.d/ubuntu.sh
+++ profile.d/ubuntu.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-type -t gconftool-2 >/dev/null 2>&1 || return;
 ################################################################################
 # Enable window resize with mouse right click
 # for 12.04
-gconftool-2 -s -t bool /apps/metacity/general/resize_with_right_button true
-# and for 12.10/13.04
+if type -t gconftool-2 >/dev/null 2>&1;then
+  gconftool-2 -s -t bool /apps/metacity/general/resize_with_right_button true
+fi
+# and for 12.10 ~ 18.04
 if [[ -x $(which gsettings) ]];then
   # Enable window resize with mouse right click
   gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+
   # Enable minimize/restore window with unity icon
-  gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+  version=$(grep DISTRIB_RELEASE /etc/lsb-release | cut -f2 -d=)
+  if (( $(echo "18.04 <= $version" | bc -l) ));then
+    gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+  else
+    gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+  fi
 fi
