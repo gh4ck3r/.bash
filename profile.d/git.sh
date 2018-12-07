6b3e00484df3a714e714e22f3f05ee8c374375df
Changbin Park <changbin_park@mcafee.com>
2018-12-07 14:58:21 +0900

    Prevent wrong git status parsing for PS1 when upstream is not exists

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -29,7 +29,7 @@ function _git_ps1()
 
   if [[ -n $GIT_PS1_SHOWDIRTYSTATE ]];then
                      #|   branch   ||       status       ||   upstream  |
-    if [[ $branch =~ ^([[:graph:]]+)([[:space:]][*+#$%]+)?([[:space:]].*)$ ]];then
+    if [[ $branch =~ ^([[:graph:]]+)([[:space:]][*+#$%]+)?([[:space:]].*)?$ ]];then
       branch=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
       local status=${BASH_REMATCH[2]}
     fi
