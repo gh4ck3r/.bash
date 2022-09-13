a351c8c9dd27fc64459629fd5986b5f69217a20f
Changbin Park <gh4ck3r@gmail.com>
2022-09-13 10:27:24 +0900

    show fzf preview only when file list(ctrl+t) is shown

--- profile.d/fzf.sh
+++ profile.d/fzf.sh
@@ -3,4 +3,4 @@
 [[ -r ~/.fzf.bash ]] && source ~/.fzf.bash
 
 export FZF_DEFAULT_OPTS='--bind ctrl-k:kill-line,ctrl-f:page-down,ctrl-b:page-up'
-[[ -x $(which bat) ]] && FZF_DEFAULT_OPTS+=' --preview "bat --style=numbers --color=always --line-range :500 {}"'
+[[ -x $(which bat) ]] && export FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
