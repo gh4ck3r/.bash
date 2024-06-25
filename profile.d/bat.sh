10b1d323797f6fd772536b28bbee3291465caf84
Changbin Park <gh4ck3r@gmail.com>
2024-06-26 00:23:34 +0900

    [bat] prevent broken ANSI from man pages

--- profile.d/bat.sh
+++ profile.d/bat.sh
@@ -3,4 +3,6 @@
 [[ -x $(which bat) ]] || return
 
 alias cat='bat --paging=never'
+
+export MANROFFOPT="-c" # https://github.com/sharkdp/bat/issues/652
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
